f5eb95d853ca790a8e584982d903c5afeb100168
Richard Willie Pham <hellorichardpham@gmail.com>
2014-03-07 12:46:54 -0800

    Modified display() method to check if the taskList is empty before printing. Otherwise, print error.
    Modified Search() method to call the delete() method in order to follow
    the intended flow of our program.

--- src/ExeCom.java
+++ src/ExeCom.java
@@ -16,6 +16,7 @@ public class ExeCom {
 	private final static String PROMPT_USER_DELETE_MESSAGE = "Select which task you would like to delete based on its Task ID Number.";
 	private final static String TASK_NOT_FOUND_MESSAGE = "That task could not be found.";
 	private final static String TASKID_NOT_FOUND_MESSAGE = "That Task ID Number was not found";
+	private final static String TASKLIST_EMPTY_MESSAGE = "There are no tasks in the task list.";
 
 	ExeCom() {
 		taskList = new ArrayList<Task>();
@@ -42,15 +43,20 @@ public class ExeCom {
 			undo();
 			return " ";
 		case "print":
-		printAllTaskInfo();
-		return " ";
+			printAllTaskInfo();
+			return " ";
 		}
 		return " ";
 	}
 
 	private static void display() {
-		for (int i = 0; i < taskList.size(); i++) {
-			System.out.println(taskList.get(i));
+		if (taskList.size() > 0) {
+			for (int i = 0; i < taskList.size(); i++) {
+				System.out.println(taskList.get(i));
+			}
+		}
+		else {
+			System.out.println(TASKLIST_EMPTY_MESSAGE);
 		}
 	}
 
@@ -71,42 +77,36 @@ public class ExeCom {
 	}
 
 	public static void delete() {
-		// Search the taskList for tasks that much the info list and display the
-		// results
-		if (search()) { // CHANGE THIS BECAUSE if(search()) doesn't make sense.
-			Scanner scan = new Scanner(System.in);
-			boolean isFound = false;
-			// The searchResults attributes now contain the tasks that matched
-			// our search
-			// Prompt user to input what line they would like to delete like in
-			// CE2
-			System.out.println(PROMPT_USER_DELETE_MESSAGE);
-			int taskIdNumber = scan.nextInt();
-			
-			for (int i = 0; i < searchResults.size(); i++) {
-				if (searchResults.get(i).getTaskID() == taskIdNumber) {
-					Task taskToDelete = searchResults.get(i);
-					saveProgress();
-					taskList.remove(taskToDelete);
-					System.out.println("Deleted: " + taskToDelete.getDetails());
-					isFound = true;
-				}
+		// Go Through search results
+		Scanner scan = new Scanner(System.in);
+		boolean isFound = false;
+		// The searchResults attributes now contain the tasks that matched
+		// our search
+		// Prompt user to input what line they would like to delete like in
+		// CE2
+		System.out.println(PROMPT_USER_DELETE_MESSAGE);
+		int taskIdNumber = scan.nextInt();
+		
+		for (int i = 0; i < searchResults.size(); i++) {
+			if (searchResults.get(i).getTaskID() == taskIdNumber) {
+				Task taskToDelete = searchResults.get(i);
+				saveProgress();
+				taskList.remove(taskToDelete);
+				System.out.println("Deleted: " + taskToDelete.getDetails());
+				isFound = true;
 			}
-			
-			if (!isFound) {
-				System.out.println(TASKID_NOT_FOUND_MESSAGE);
-			}
-			
-		} else {
-			System.out.println(TASK_NOT_FOUND_MESSAGE);
 		}
 
+		if (!isFound) {
+			System.out.println(TASKID_NOT_FOUND_MESSAGE);
+		}
 	}
 
 	public static void printSearch() {
 		if (!searchResults.isEmpty()) {
 			for (int i = 0; i < searchResults.size(); i++) {
-				System.out.println(searchResults.get(i).getDetails());
+				System.out.println(searchResults.get(i).getTaskID() + " "
+						+ searchResults.get(i).getDetails());
 			}
 		} else {
 			System.out.println(TASK_NOT_FOUND_MESSAGE);
@@ -119,26 +119,24 @@ public class ExeCom {
 		}
 	}
 
-	public static boolean search() {
+	public static void search() {
 		boolean isFound = false;
 		reinitializeSearchResults();
 		// Is this right? I don't think infoString is found in index 0.
 		// Definitely not right.
 		String infoString = info[1];
-		
+
 		for (int counter = 0; counter < taskList.size(); counter++) {
 			if (taskList.get(counter).getDetails().contains(infoString)) {
 				searchResults.add(taskList.get(counter));
 				isFound = true;
 			}
 		}
-		if(isFound) {
+		if (isFound) {
 			printSearch();
-			return isFound;
-		}
-		else {
+			delete();
+		} else {
 			System.out.println(TASK_NOT_FOUND_MESSAGE);
-			return isFound;
 		}
 	}
 
@@ -171,5 +169,10 @@ public class ExeCom {
 			System.out.println("added to taskList: "
 					+ prevTaskList.get(counter));
 		}
+	}
+
+	public static void printAllTaskInfo() {
+		for (int i = 0; i < info.length; i++)
+			System.out.print(info[i] + " ");
 	}
 }

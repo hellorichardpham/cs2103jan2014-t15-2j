916866a33209c1d73fb8a6addb27d8ce660af29c
Richard Willie Pham <hellorichardpham@gmail.com>
2014-03-13 00:44:47 -0700

    Removed the DELETE case from the switch statement in executeCommand because we will never explicitly say "Delete (keyword)"

--- src/ExeCom.java
+++ src/ExeCom.java
@@ -8,7 +8,6 @@ public class ExeCom {
 	private static String[] info;
 	private final static String ADD = "add";
 	private final static String DISPLAY = "display";
-	private final static String DELETE = "delete";
 	private final static String SEARCH = "search";
 	private final static String UNDO = "undo";
 	private final static String UNDO_SUCCESS_MESSAGE = "Action has successfully been undone.";
@@ -35,10 +34,6 @@ public class ExeCom {
 			return " ";
 		case DISPLAY:
 			display();
-			return " ";
-		case DELETE:
-			delete();
-			s.saveStorage();
 			return " ";
 		case SEARCH:
 			search();

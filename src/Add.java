f747a733e47e06565bc23cb34ca8d249e72ef58f
Tan Ying Yun <tyingyun@hotmail.com>
2014-03-27 12:23:27 +0800

    Edited printSearch to print TaskID to aid user in deleting/updated searched tasks

---
+++ src/Add.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+
+public class Add {
+	private final static String ADD_SUCCESSFUL_MESSAGE = "That task has successfully been added to the Task List.";
+	private ArrayList<Task> taskList;
+
+	//constructor
+	public Add(ArrayList<Task> taskList){
+		this.taskList = taskList;
+		
+	}
+	
+	/**
+	 * 
+	 * addToTaskList: Add tasks to arrayList and set taskID.
+	 * 
+	 * @author Richard
+	 * @param void
+	 * @return void
+	 */
+	public void addToTaskList(Command c) throws Exception {
+		
+		{
+			Task taskToAdd = new Task(c);
+			
+			taskToAdd.setTaskID(Integer.toString(ExeCom.getTaskListInstance().size() + 1));
+			taskList.add(taskToAdd);
+			
+			System.out.println(ADD_SUCCESSFUL_MESSAGE);
+		}
+	}
+	
+
+}//end class
---
+++ src/Display.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class Display {
+	
+private final static String TASKLIST_EMPTY_MESSAGE = "There are no tasks in the task list.";
+private static final CharSequence EMPTY_STRING = "";
+private ArrayList<Task> taskList;
+	
+	//constructor
+ 	public Display(ArrayList<Task> taskList){
+		this.taskList = taskList;
+	}
+ 	
+	/**
+	 * 
+	 * display: display all tasks found in the taskList
+	 * 
+	 * @author Khaleef
+	 * @param void
+	 * @return void
+	 */
+	public void displayTaskList() {
+		if (!taskList.isEmpty()) {
+			printListingHeader();
+			for (Task task : taskList) {
+				String print = task.displayTask();
+				printTaskIndex(task, print);
+			}
+		} else if (taskList.isEmpty()) {
+			System.out.println(TASKLIST_EMPTY_MESSAGE);
+		} 
+	}
+	
+	/**
+	 * 
+	 * printListingHeader: print header for listing
+	 * 
+	 * @author Ying Yun
+	 * @param void
+	 * @return void
+	 */
+	private void printListingHeader() {
+		System.out.println("~~~~~ Listing of all tasks ~~~~~");
+	}
+	
+	
+	/**
+	 * 
+	 * printTaskIndex: print index number of current task
+	 * 
+	 * @author Ying Yun
+	 * @param Task, String
+	 * @return void
+	 */
+	private void printTaskIndex(Task task, String print) {
+		System.out.println((taskList.indexOf(task)+1) + ": " + print);
+	}
+	
+	
+	/**
+	 * 
+	 * printTaskIndex: print index number of current task
+	 * 
+	 * @author Richard
+	 * @param String
+	 * @return String
+	 */
+	public String replaceNull(String print) {
+		print = print.replace("null ", EMPTY_STRING);
+		print = print.replace("null", EMPTY_STRING);
+		return print;
+	}
+}//end class

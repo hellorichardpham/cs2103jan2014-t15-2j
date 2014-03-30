98bbabc1679d6fc6c32dedf440204e590f8aef92
khaleef1990 <khaleef1990@localhost>
2014-03-30 10:51:49 +0800

    amend storage and email class

--- src/Email.java
+++ src/Email.java
@@ -3,6 +3,19 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.ColumnText;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+import java.io.*;
+
 public class Email {
 
 	private Scanner input;
@@ -50,15 +63,78 @@ public class Email {
 
 		messageBody = sb.toString();
 		br.close();
-		
+
+		String storageName = "Storage.txt";
+		String pdfName = "Storage.pdf";
+
+		taskList = ExeCom.getTaskListInstance();
+
+		System.out.println("Sending email. Please wait.");
+		// if (new MailUtil().sendMail(recipients, subject, messageBody, name,
+		// email, password, host))
+		// System.out.println("Email sent. Thank you for waiting.");
+
 		try {
-			System.out.println("Sending email. Please wait.");
-			if (new MailUtil().sendMail(recipients, subject, messageBody, name,
-					email, password, host))
-				System.out.println("Email sent. Thank you for waiting.");
+				
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
-
 	}
+	
+	public void createPdf(String filename)
+	        throws IOException, DocumentException {
+	    	// step 1
+	        Document document = new Document();
+	        // step 2
+	        PdfWriter.getInstance(document, new FileOutputStream(filename));
+	        // step 3
+	        document.open();
+	        // step 4
+	        PdfPTable table = createTable1();
+	        document.add(table);
+	        table = createTable2();
+	        table.setSpacingBefore(5);
+	        table.setSpacingAfter(5);
+	        document.add(table);
+	        // step 5
+	        document.close();
+	    }
+	
+	public static PdfPTable createTable1() throws DocumentException {
+        PdfPTable table = new PdfPTable(3);
+        table.setWidthPercentage(288 / 5.23f);
+        table.setWidths(new int[]{2, 1, 1});
+        PdfPCell cell;
+        cell = new PdfPCell(new Phrase("Table 1"));
+        cell.setColspan(3);
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase("Cell with rowspan 2"));
+        cell.setRowspan(2);
+        table.addCell(cell);
+        table.addCell("row 1; cell 1");
+        table.addCell("row 1; cell 2");
+        table.addCell("row 2; cell 1");
+        table.addCell("row 2; cell 2");
+        return table;
+    }
+	
+	public static PdfPTable createTable2() throws DocumentException {
+        PdfPTable table = new PdfPTable(3);
+        table.setTotalWidth(288);
+        table.setLockedWidth(true);
+        table.setWidths(new float[]{2, 1, 1});
+        PdfPCell cell;
+        cell = new PdfPCell(new Phrase("Table 2"));
+        cell.setColspan(3);
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase("Cell with rowspan 2"));
+        cell.setRowspan(2);
+        table.addCell(cell);
+        table.addCell("row 1; cell 1");
+        table.addCell("row 1; cell 2");
+        table.addCell("row 2; cell 1");
+        table.addCell("row 2; cell 2");
+        return table;
+    }
+	
 }

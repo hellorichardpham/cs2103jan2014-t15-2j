5e1561ef9c2f92139432bd265989a1ef895cf660
khaleef1990 <khaleef1990@localhost>
2014-03-30 10:51:49 +0800

    amend storage and email class

--- src/Storage.java
+++ src/Storage.java
@@ -9,6 +9,21 @@ import java.util.logging.*;
 public class Storage {
 
 	private static final String FILENAME = "Storage.txt";
+	private static final String LOG_FILE_NAME = "MyLogFile.txt";
+	private static final String LOG_START = "Start of retrieving tasks";
+	private static final String LOG_ERROR = "Processing error";
+	private static final String LOG_END = "Retrieve task successful";
+	private static final String EQUAL_LOCATION = "//location";
+	private static final String EQUAL_CATEGORY = "//category";
+	private static final String EQUAL_PRIORITY = "//priority";
+	private static final String EQUAL_TASKID = "//taskid";
+	private static final String EQUAL_BLANK_SPACE = " ";
+	private static final String EQUAL_EMPTY_STRING = "";
+	
+	private static final int ZERO_CONST = 0;
+	private static final int ONE_CONST = 1;
+	private static final int TEN_CONST = 10;
+
 	private static Logger logger = Logger.getLogger("MyLog");
 
 	/**
@@ -26,113 +41,154 @@ public class Storage {
 		// to disable log message on output console
 		logger.setUseParentHandlers(false);
 		// log message written to MyLogFile.txt
-		fh = new FileHandler("MyLogFile.txt");
+		fh = new FileHandler(LOG_FILE_NAME);
 
 		BufferedReader fileReader;
 		fileReader = createFileIfNotExist();
 
 		String[] retrieve;
 		int arraySize;
-		// int content;
-		// int setContent;
 		int counter;
 		String text;
 		try {
 			logger.addHandler(fh);
 			SimpleFormatter formatter = new SimpleFormatter();
 			fh.setFormatter(formatter);
-			logger.log(Level.INFO, "Start of retrieving tasks");
+			logger.log(Level.INFO, LOG_START);
 
 			while ((text = fileReader.readLine()) != null) {
 				Task task = new Task();
 
-				String location = "";
-				String category = "";
-				String priority = "";
+				String location = EQUAL_EMPTY_STRING;
+				String category = EQUAL_EMPTY_STRING;
+				String priority = EQUAL_EMPTY_STRING;
+				String details = EQUAL_EMPTY_STRING;
 				int i;
-				int endPosition = 0;
-				int startPosition = 0;
+				int endPosition = ZERO_CONST;
+				int startPosition = ZERO_CONST;
 
-				retrieve = text.split(" ");
+				retrieve = text.split(EQUAL_BLANK_SPACE);
 				arraySize = retrieve.length;
 
-				for (i = 0; i < arraySize; i++) {
-					if (retrieve[i].equals("//location")) {
+				for (i = ZERO_CONST; i < arraySize; i++) {
+					if (retrieve[i].equals(EQUAL_LOCATION)) {
 						endPosition = i;
-						for (int j = 1; j < arraySize; j++) {
-							if (retrieve[j + i].equals("//category"))
+						for (int j = ONE_CONST; j < arraySize; j++) {
+							if (retrieve[j + i].equals(EQUAL_CATEGORY))
 								break;
 							location += retrieve[j + i] + " ";
 						}
 					}
-					if (retrieve[i].equals("//category")) {
-						for (int j = 1; j < arraySize; j++) {
-							if (retrieve[j + i].equals("//priority"))
+					if (retrieve[i].equals(EQUAL_CATEGORY)) {
+						for (int j = ONE_CONST; j < arraySize; j++) {
+							if (retrieve[j + i].equals(EQUAL_PRIORITY))
 								break;
 							category += retrieve[j + i] + " ";
 						}
 					}
-					if (retrieve[i].equals("//priority")) {
-						for (int j = 1; j < arraySize; j++) {
-							if (retrieve[j + i].equals("//taskid"))
+					if (retrieve[i].equals(EQUAL_PRIORITY)) {
+						for (int j = ONE_CONST; j < arraySize; j++) {
+							if (retrieve[j + i].equals(EQUAL_TASKID))
 								break;
 							priority += retrieve[j + i] + " ";
 						}
 					}
 				}
-				if (location.endsWith(" "))
-					location = location.substring(0, location.length() - 1);
-				if (category.endsWith(" "))
-					category = category.substring(0, category.length() - 1);
-				if (priority.endsWith(" "))
-					priority = priority.substring(0, priority.length() - 1);
+				if (location.endsWith(EQUAL_BLANK_SPACE))
+					location = location.substring(ZERO_CONST, location.length() - ONE_CONST);
+				if (category.endsWith(EQUAL_BLANK_SPACE))
+					category = category.substring(ZERO_CONST, category.length() - ONE_CONST);
+				if (priority.endsWith(EQUAL_BLANK_SPACE))
+					priority = priority.substring(ZERO_CONST, priority.length() - ONE_CONST);
 
-				task.setLocation(location);
-				task.setCategory(category);
-				task.setPriority(priority);
-				task.setTaskID(retrieve[arraySize - 1]);
+				setLocationToTaskIDAttribute(retrieve, arraySize, task,
+						location, category, priority);
 
-				startPosition = endPosition - 10;
+				startPosition = endPosition - TEN_CONST;
 				endPosition = startPosition;
 
-				task.setStartDay(retrieve[startPosition]);
-				startPosition++;
-				task.setStartMonth(retrieve[startPosition]);
-				startPosition++;
-				task.setStartYear(retrieve[startPosition]);
-				startPosition++;
-				task.setEndDay(retrieve[startPosition]);
-				startPosition++;
-				task.setEndMonth(retrieve[startPosition]);
-				startPosition++;
-				task.setEndYear(retrieve[startPosition]);
-				startPosition++;
-				task.setStartHours(retrieve[startPosition]);
-				startPosition++;
-				task.setStartMins(retrieve[startPosition]);
-				startPosition++;
-				task.setEndHours(retrieve[startPosition]);
-				startPosition++;
-				task.setEndMins(retrieve[startPosition]);
+				setDayToMinsAttributes(retrieve, task, startPosition);
 
-				String details = "";
-
-				for (counter = 0; counter < endPosition; counter++) {
-					if (counter == endPosition - 1) {
+				for (counter = ZERO_CONST; counter < endPosition; counter++) {
+					if (counter == endPosition - ONE_CONST) {
 						details += retrieve[counter];
 					} else {
-						details += retrieve[counter] + " ";
+						details += retrieve[counter] + EQUAL_BLANK_SPACE;
 					}
 				}
-				task.setDetails(details);
+				
+				setDetailsAttibutes(task, details);
 				ExeCom.getTaskListInstance().add(task);
 			}
 			fileReader.close();
 		} catch (Exception ex) {
-			logger.log(Level.WARNING, "Processing error", ex);
+			logger.log(Level.WARNING, LOG_ERROR, ex);
 		}
-		logger.log(Level.INFO, "Retrieve task successful");
+		logger.log(Level.INFO, LOG_END);
 		fh.close();
+	}
+
+	/**
+	 * 
+	 * setDetailsAttibutes: set attributes for details into arrayList
+	 * 
+	 * @author Khaleef
+	 * @param Task, String
+	 * @return void
+	 */
+	private void setDetailsAttibutes(Task task, String details) {
+		task.setDetails(details);
+	}
+	
+	/**
+	 * 
+	 * setLocationToTaskIDAttribute: set attributes for location, category, priority, taskID
+	 * into arrayList
+	 * 
+	 * @author Khaleef
+	 * @param String[], int, Task, String, String, String
+	 * @return void
+	 */
+	private void setLocationToTaskIDAttribute(String[] retrieve, int arraySize,
+			Task task, String location, String category, String priority) {
+		task.setLocation(location);
+		task.setCategory(category);
+		task.setPriority(priority);
+		task.setTaskID(retrieve[arraySize - 1]);
+	}
+
+	/**
+	 * 
+	 * setDayToMinsAttributes: set attributes for startDay, startMonth, startYear, 
+	 * endDay, endMonth, endYear, startHours, startMins, endHours, endMins
+	 * into arrayList
+	 * to taskList
+	 * 
+	 * @author Khaleef
+	 * @param String[], Task, int
+	 * @return void
+	 */
+	private void setDayToMinsAttributes(String[] retrieve, Task task,
+			int startPosition) {
+		task.setStartDay(retrieve[startPosition]);
+		startPosition++;
+		task.setStartMonth(retrieve[startPosition]);
+		startPosition++;
+		task.setStartYear(retrieve[startPosition]);
+		startPosition++;
+		task.setEndDay(retrieve[startPosition]);
+		startPosition++;
+		task.setEndMonth(retrieve[startPosition]);
+		startPosition++;
+		task.setEndYear(retrieve[startPosition]);
+		startPosition++;
+		task.setStartHours(retrieve[startPosition]);
+		startPosition++;
+		task.setStartMins(retrieve[startPosition]);
+		startPosition++;
+		task.setEndHours(retrieve[startPosition]);
+		startPosition++;
+		task.setEndMins(retrieve[startPosition]);
 	}
 
 	/**
@@ -169,7 +225,7 @@ public class Storage {
 		currentFile.delete();
 		PrintWriter pw = new PrintWriter(new FileOutputStream(FILENAME));
 		for (Task t : ExeCom.getTaskListInstance()) {
-			pw.println(t.displayToStorage() + "//taskid "
+			pw.println(t.displayToStorage() + EQUAL_TASKID + EQUAL_BLANK_SPACE
 					+ retrieveCurrentTaskID(t));
 
 		}

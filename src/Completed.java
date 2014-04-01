e084719f2b9561335134f84c3d72c207a587bff2
Tian Weizhou <Tian Weizhou@localhost>
2014-04-02 00:58:39 +0800

    shifted print statements to ExeCom

--- src/Completed.java
+++ src/Completed.java
@@ -9,28 +9,31 @@ public class Completed {
 		taskList = ExeCom.getTaskListInstance();
 	}
 	
-	public void markCompleted(Command c) {
+	public String markCompleted(Command c) {
+                String feedback = "";
 		for(String target : c.getTargetedTasks()){
-			markSpecificTaskCompleted(target);
+			feedback = feedback + markSpecificTaskCompleted(target);
 		}
+                return feedback;
 	}
 
-	private void markSpecificTaskCompleted(String target) {
+	private String markSpecificTaskCompleted(String target) {
 		ExeCom ec = new ExeCom();
 		int taskIdNumber = ec.retrieveTaskIdNumber(target);
 		boolean isFound = false;
+                String output = "";
 		//loop thru whole taskList to find for the user specified task
 		for (int i = 0; i < taskList.size(); i++) {
 			if (ec.isTaskIDMatch(taskList.get(i).getTaskID(), taskIdNumber)) {
-				System.out.println("This task is marked as completed: " + taskList.get(i).getDetails());
+				output = output + "This task is marked as completed: " + taskList.get(i).getDetails() + "\n";
 				Task task = taskList.get(i);
 				task.setCompleted("true");
 				isFound = true;
 			}
 		}
 		if (!isFound) {
-			System.out.println(TASKID_NOT_FOUND_MESSAGE);
+			output = TASKID_NOT_FOUND_MESSAGE;
 		}
-		
+		return output;
 	}
 }

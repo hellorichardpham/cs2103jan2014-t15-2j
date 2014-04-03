e2f122cceb7e17e5d18a284c218ec98586f9247a
Richard Willie Pham <hellorichardpham@gmail.com>
2014-04-03 19:20:10 +0800

    Fixed problem where undo and redo button were not working.

--- src/TaskTrackerGUI.java
+++ src/TaskTrackerGUI.java
@@ -9,13 +9,14 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	//code placed at front of feedback for GUI to recognize there is a conflict
+	// code placed at front of feedback for GUI to recognize there is a conflict
 	private final static String CONFLICTED_CODE = "-cs2103--conflicted";
 	private final static String CANCELLED = "cancel";
 	private String conflictedUserInput;
-	
+
 	/**
 	 * Creates new form TaskTrackerGUI
+	 * 
 	 * @author Tian Weizhou
 	 */
 	public TaskTrackerGUI() {
@@ -24,6 +25,7 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 
 	/**
 	 * This method is called from within the constructor to initialize the form.
+	 * 
 	 * @author Tian Weizhou
 	 */
 	// <editor-fold defaultstate="collapsed"
@@ -82,40 +84,40 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 				.addGroup(
 						layout.createSequentialGroup()
-						.addContainerGap()
-						.addGroup(
-								layout.createParallelGroup(
-										javax.swing.GroupLayout.Alignment.LEADING)
-										.addComponent(
-												jScrollPane1,
-												javax.swing.GroupLayout.Alignment.TRAILING)
+								.addContainerGap()
+								.addGroup(
+										layout.createParallelGroup(
+												javax.swing.GroupLayout.Alignment.LEADING)
+												.addComponent(
+														jScrollPane1,
+														javax.swing.GroupLayout.Alignment.TRAILING)
 												.addGroup(
 														layout.createSequentialGroup()
-														.addComponent(
-																undoButton)
+																.addComponent(
+																		undoButton)
 																.addGap(18, 18,
 																		18)
-																		.addComponent(
-																				redoButton)
-																				.addPreferredGap(
-																						javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-																						662,
-																						Short.MAX_VALUE)
-																						.addComponent(
-																								exitButton))
-																								.addGroup(
-																										javax.swing.GroupLayout.Alignment.TRAILING,
-																										layout.createSequentialGroup()
-																										.addComponent(
-																												jLabel1,
-																												javax.swing.GroupLayout.PREFERRED_SIZE,
-																												101,
-																												javax.swing.GroupLayout.PREFERRED_SIZE)
-																												.addPreferredGap(
-																														javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-																														.addComponent(
-																																commandLine)))
-																																.addContainerGap()));
+																.addComponent(
+																		redoButton)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.RELATED,
+																		662,
+																		Short.MAX_VALUE)
+																.addComponent(
+																		exitButton))
+												.addGroup(
+														javax.swing.GroupLayout.Alignment.TRAILING,
+														layout.createSequentialGroup()
+																.addComponent(
+																		jLabel1,
+																		javax.swing.GroupLayout.PREFERRED_SIZE,
+																		101,
+																		javax.swing.GroupLayout.PREFERRED_SIZE)
+																.addPreferredGap(
+																		javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+																.addComponent(
+																		commandLine)))
+								.addContainerGap()));
 
 		layout.linkSize(javax.swing.SwingConstants.HORIZONTAL,
 				new java.awt.Component[] { exitButton, redoButton, undoButton });
@@ -124,10 +126,10 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
 				.addGroup(
 						layout.createSequentialGroup()
-						.addContainerGap()
-						.addComponent(jScrollPane1,
-								javax.swing.GroupLayout.DEFAULT_SIZE,
-								363, Short.MAX_VALUE)
+								.addContainerGap()
+								.addComponent(jScrollPane1,
+										javax.swing.GroupLayout.DEFAULT_SIZE,
+										363, Short.MAX_VALUE)
 								.addGap(18, 18, 18)
 								.addGroup(
 										layout.createParallelGroup(
@@ -137,21 +139,23 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 														javax.swing.GroupLayout.PREFERRED_SIZE,
 														javax.swing.GroupLayout.DEFAULT_SIZE,
 														javax.swing.GroupLayout.PREFERRED_SIZE)
-														.addComponent(jLabel1))
-														.addGap(18, 18, 18)
-														.addGroup(
-																layout.createParallelGroup(
-																		javax.swing.GroupLayout.Alignment.BASELINE)
-																		.addComponent(undoButton)
-																		.addComponent(redoButton)
-																		.addComponent(exitButton))
-																		.addContainerGap()));
+												.addComponent(jLabel1))
+								.addGap(18, 18, 18)
+								.addGroup(
+										layout.createParallelGroup(
+												javax.swing.GroupLayout.Alignment.BASELINE)
+												.addComponent(undoButton)
+												.addComponent(redoButton)
+												.addComponent(exitButton))
+								.addContainerGap()));
 
 		pack();
 	}// </editor-fold>//GEN-END:initComponents
-	
+
 	/**
-	 * This line describes the action taken by the program when the user enters a line of command
+	 * This line describes the action taken by the program when the user enters
+	 * a line of command
+	 * 
 	 * @author Tian Weizhou
 	 */
 	private void commandLineActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_commandLineActionPerformed
@@ -160,19 +164,20 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 		ProcessCommand pc = new ProcessCommand();
 		ExeCom ec = ExeCom.getInstance();
 		Command com = pc.process(userInput);
-		if(conflictedUserInput!=null) {
-		switch(userInput.toLowerCase()) {
-		case "yes":
-		case "y":
-		case "ya":
+		if (conflictedUserInput != null) {
+			switch (userInput.toLowerCase()) {
+			case "yes":
+			case "y":
+			case "ya":
 				com = pc.process(conflictedUserInput);
-				conflictedUserInput=null;
+				System.out.println(com);
+				conflictedUserInput = null;
 				break;
-		case "no":
-		case "n":
-			com.setKeyword(CANCELLED);
-			
-		}
+			case "no":
+			case "n":
+				com.setKeyword(CANCELLED);
+
+			}
 		}
 
 		try {
@@ -180,42 +185,60 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		String feedback;
 		feedback = ExeCom.getFeedback();
 
-		if(feedback.contains(CONFLICTED_CODE)) {
-			//change command keyword to justadd
-			conflictedUserInput ="just" + userInput;
+		if (feedback.contains(CONFLICTED_CODE)) {
+			// change command keyword to justadd
+			conflictedUserInput = "just" + userInput;
 			feedback = feedback.replace(CONFLICTED_CODE, "").trim();
 		}
 		displayTextBox.append(feedback);
-		
+
 	}// GEN-LAST:event_commandLineActionPerformed
-	
+
 	/**
-	 * This line describes the action taken by the program when the user clicks on the Undo button
+	 * This line describes the action taken by the program when the user clicks
+	 * on the Undo button
+	 * 
 	 * @author Tian Weizhou
 	 */
 	private void undoButtonActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_undoButtonActionPerformed
-		ExeCom.undo();
+		ExeCom ec = ExeCom.getInstance();
+		Command command = new Command();
+		command.setKeyword("undo");
+		try {
+			ec.executeCommand(command);
+		} catch (Exception e) {
+			System.out.println("Could not perform undo command");
+		}
 		String feedback = ExeCom.getFeedback();
 		displayTextBox.append(feedback);
 	}// GEN-LAST:event_undoButtonActionPerformed
-	
+
 	/**
-	 * This line describes the action taken by the program when the user clicks on the Redo button
-	 *
+	 * This line describes the action taken by the program when the user clicks
+	 * on the Redo button
+	 * 
 	 * @author Tian Weizhou
 	 */
 	private void redoButtonActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_redoButtonActionPerformed
-		ExeCom.redo();
+		ExeCom ec = ExeCom.getInstance();
+		Command command = new Command();
+		command.setKeyword("redo");
+		try {
+			ec.executeCommand(command);
+		} catch (Exception e) {
+			System.out.println("Could not perform redo command");
+		}
 		String feedback = ExeCom.getFeedback();
 		displayTextBox.append(feedback);
 	}// GEN-LAST:event_redoButtonActionPerformed
-	
+
 	/**
 	 * This line closes the program when the user clicks on the Exit button
+	 * 
 	 * @author Tian Weizhou
 	 */
 	private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_exitButtonActionPerformed
@@ -246,16 +269,16 @@ public class TaskTrackerGUI extends javax.swing.JFrame {
 			}
 		} catch (ClassNotFoundException ex) {
 			java.util.logging.Logger.getLogger(TaskTrackerGUI.class.getName())
-			.log(java.util.logging.Level.SEVERE, null, ex);
+					.log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (InstantiationException ex) {
 			java.util.logging.Logger.getLogger(TaskTrackerGUI.class.getName())
-			.log(java.util.logging.Level.SEVERE, null, ex);
+					.log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (IllegalAccessException ex) {
 			java.util.logging.Logger.getLogger(TaskTrackerGUI.class.getName())
-			.log(java.util.logging.Level.SEVERE, null, ex);
+					.log(java.util.logging.Level.SEVERE, null, ex);
 		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
 			java.util.logging.Logger.getLogger(TaskTrackerGUI.class.getName())
-			.log(java.util.logging.Level.SEVERE, null, ex);
+					.log(java.util.logging.Level.SEVERE, null, ex);
 		}
 		// </editor-fold>
 

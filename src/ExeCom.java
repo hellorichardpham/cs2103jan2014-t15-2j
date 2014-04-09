eb5915cc4ad6f4fbca180b0e0f1618db7f25fa77
Tian Weizhou <Tian Weizhou@localhost>
2014-04-10 03:40:51 +0800

    added functionality to check if undo and redo are possible

--- src/ExeCom.java
+++ src/ExeCom.java
@@ -805,4 +805,12 @@ public class ExeCom {
 	public int retrieveTaskIdNumber(String taskID) {
 		return Integer.parseInt(taskID);
 	}
+
+	public static boolean checkUndoStack() {
+		return !undoStack.empty();
+	}
+	
+	public static boolean checkRedoStack() {
+		return !redoStack.empty();
+	}
 }

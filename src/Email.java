29e54f73f0854c339feac3e4551a2147508057e6
Tian Weizhou <Tian Weizhou@localhost>
2014-04-14 22:48:50 +0800

    added function to set Email Address

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 //@author A0097961M
 
 public class Email {
@@ -8,11 +14,41 @@ public class Email {
 	private static final String DETAILS = "Your notifications details is as follow:\n";
 	private static final String REGARDS = "Regards,\nTaskTracker";
 	private static final String SUBJECT = "Reminder From TaskTracker";
-	private static final String EMAIL_K = "khaleef1990@gmail.com";
-	private static final String EMAIL_R = "hellorichardpham@gmail.com";
-	private static final String EMAIL_Y = "tyingyun@hotmail.com";
-	private static final String EMAIL_J = "tianweizhou@gmail.com";
-
+	
+	private static String userEmail;
+	private static Email theEmail;
+	
+	public static Email getTheEmailInstance() throws FileNotFoundException {
+		if(theEmail == null) {
+			theEmail = new Email();
+			userEmail = "";
+		}
+		BufferedReader input;
+		try {
+			input = new BufferedReader(new FileReader("emailAddress.txt"));
+		} catch (FileNotFoundException e) {
+			PrintWriter writer = new PrintWriter("emailAddress.txt");
+			input = new BufferedReader(new FileReader("emailAddress.txt"));
+			writer.close();
+		}
+		try {
+			userEmail = input.readLine();
+		} catch (IOException e) {
+		}
+		return theEmail;
+	}
+	
+	//A0083093E
+	//constructor
+	public static void setUserEmail(String userEm) {
+		userEmail = userEm;
+	}
+	
+	//A0083093E
+	public static String getUserEmail() {
+		return userEmail;
+	}
+	
 	// @author A0097961M
 	/*
 	 * emailUser: set up of the email name, host, password, recipients
@@ -21,7 +57,7 @@ public class Email {
 	 * 
 	 * @return void
 	 */
-	public static void emailUser(String task_Info) throws Exception {
+	public void emailUser(String task_Info) throws Exception {
 		String name = NAME;
 		String email = EMAIL;
 		String password = PASSWORD;
@@ -37,7 +73,7 @@ public class Email {
 		// }
 
 		// List of recipients to email
-		String[] recipients = new String[] { EMAIL_K, EMAIL_R, EMAIL_Y, EMAIL_J };
+		String[] recipients = new String[] { userEmail };
 
 		String subject = SUBJECT;
 
